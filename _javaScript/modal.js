56c7996b763d9a7a6ddf1ca4a03ace10572189be
wellington Monteiro <wellington_log@hotmail.com.br>
2021-08-21 20:02:13 -0300

    Primeiro commit

---
+++ _javaScript/modal.js
@@ -0,0 +1,9 @@
+function abrirModal(){
+    document.getElementById('modal').style.top="0";
+    document.getElementById('modal').style.display="block";
+}
+function fecharModal(){
+    document.getElementById('modal').style.top="-100%";
+    document.getElementById('modal').style.display="none";
+}
+
